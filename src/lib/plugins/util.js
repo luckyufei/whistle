2dd9da28607183f52da46e8ea7781fdfc02f5542
allanyu <allanyu@tencent.com>
2022-05-15 06:58:36 +0800

    [ts-migrate][src] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

---
+++ src/lib/plugins/util.js
@@ -0,0 +1,79 @@
+var util = require('../util');
+var config = require('../config');
+var protocols = require('../rules/protocols');
+
+var ORG_RE = /^@[\w-]+$/;
+var WHISLTE_PLUGIN_RE = /^whistle\.[a-z\d_\-]+$/;
+var HTTP_RE = /^https?:\/\//i;
+var PLUGIN_NAME_RE = /^(?:@[\w-]+\/)?whistle\.[a-z\d_\-]+$/;
+
+function isOrgModule(name) {
+  return ORG_RE.test(name);
+}
+
+exports.isOrgModule = isOrgModule;
+
+exports.isPluginName = function(name) {
+  return PLUGIN_NAME_RE.test(name);
+};
+
+function isWhistleModule(name) {
+  return WHISLTE_PLUGIN_RE.test(name);
+}
+
+exports.isWhistleModule = isWhistleModule;
+
+function getHomePageFromPackage(pkg) {
+  if (HTTP_RE.test(pkg.homepage)) {
+    return pkg.homepage;
+  }
+
+  return extractUrl(pkg.repository) || '';
+}
+
+function extractUrl(repository) {
+  if (
+    !repository ||
+    repository.type != 'git' ||
+    typeof repository.url != 'string'
+  ) {
+    return;
+  }
+
+  var url = repository.url.replace(/^git\+/i, '');
+  if (!HTTP_RE.test(url)) {
+    url = url.replace(/^git@([^:]+):/, 'http://$1/');
+  }
+
+  return url.replace(/\.git\s*$/i, '');
+}
+
+exports.getHomePageFromPackage = getHomePageFromPackage;
+
+exports.parseValues = function (val) {
+  if (val) {
+    val = util.parseJSON(val);
+  }
+  if (!val) {
+    return '';
+  }
+  Object.keys(val).forEach(function (key) {
+    val[key] = util.toString(val[key]);
+  });
+  return val;
+};
+
+exports.getPluginHomepage = function (pkg) {
+  var url = pkg.pluginHomepage || pkg.pluginHomePage;
+  return typeof url === 'string' ? url : undefined;
+};
+
+exports.excludePlugin = function (name) {
+  if (
+    protocols.contains(name) ||
+    (config.allowPluginList && config.allowPluginList.indexOf(name) === -1)
+  ) {
+    return true;
+  }
+  return config.blockPluginList && config.blockPluginList.indexOf(name) !== -1;
+};
